fa86cbcb243bf03241725bfded4a2bf3ca6a457c
Jon Higgins <jon@jonhiggins.co.uk>
2018-04-23 14:04:21 +1000

    type layout start

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'react-emotion'
+
 import spacing from '~/src/settings/spacing'
-import { interUIStyles, vollkornStyles } from '~/src/settings/typography'
+import { interUIStyles, vollkornStyles, BASELINE } from '~/src/settings/typography'
+
+const styleBlocks = [interUIStyles, vollkornStyles]
+
+const Columns = styled('ul')`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+`
+
+const Column = styled('li')`
+  flex: 0 0 50%;
+`
+
+const TypeBlock = styled('p')`
+  margin: 0;
+  height: ${BASELINE * 6};
+`
+
 const IndexPage = () => (
   <div>
-    {interUIStyles.map((item, index) => (<p key={index} style={item}>Lorem ipsum</p>))}
-    {vollkornStyles.map((item, index) => (<p key={index} style={item}>Lorem ipsum</p>))}
+    <Columns>
+      {styleBlocks.map((block, index) => (
+        <Column key={index}>
+          {block.map((item, childIndex) => (
+            <TypeBlock key={childIndex} style={item}>
+              {item.fontSizeRaw} / {item.lineHeightRaw}
+            </TypeBlock>
+          ))}
+        </Column>
+      ))}
+    </Columns>
   </div>
 )
 
