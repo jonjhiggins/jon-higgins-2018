3f6d8de3bde272f9bec2bb4783443494b4d4d7b6
Jon Higgins <jon@jonhiggins.co.uk>
2018-04-26 13:53:18 +1000

    upgrade to react 16

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,6 @@ module.exports = {
   siteMetadata: {
     title: 'Jon Higgins - a front-end developer based in Melbourne, Australia',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-emotion'],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-emotion', 'gatsby-plugin-react-next'],
   pathPrefix: `/jon-higgins-2018`,
 }
