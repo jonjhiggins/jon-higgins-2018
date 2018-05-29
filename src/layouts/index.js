85886abec4c76072460065c6d635551c1d2aa633
Jon Higgins <jon@jonhiggins.co.uk>
2018-05-29 22:24:49 +1000

    control baseline grid from URL query

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+// Polyfills
+import 'url-search-params-polyfill'
 
 import BaselineGrid from '~/src/components/baseline-grid'
 // eslint-disable-next-line no-unused-vars
@@ -13,28 +15,34 @@ import SiteHeader from '~/src/components/site-header'
 // import vollkorn from '~/src/fonts/vollkorn.woff2'
 // import vollkornBold from '~/src/fonts/vollkorn-bold.woff2'
 
-const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: '@TODO' },
-        { name: 'keywords', content: '@TODO' },
-      ]}
-    >
-      {/* <link rel="preload" href={interUI} as="font" type="font/woff2" />
-      <link rel="preload" href={interUIBold} as="font" type="font/woff2" />
-      <link rel="preload" href={vollkorn} as="font" type="font/woff2" />
-      <link rel="preload" href={vollkornBold} as="font" type="font/woff2" /> */}
-    </Helmet>
-    <BaselineGrid />
-    <SiteHeader
-      titleHTML={data.site.siteMetadata.titleHTML}
-      navLinks={data.site.navLinks}
-    />
-    {children()}
-  </div>
-)
+const Layout = ({ children, data, location }) => {
+  const search = location.search
+  const params = new URLSearchParams(search)
+  const grid = params.get('grid')
+  const hasGrid = grid === 'true' || grid === '1'
+  return (
+    <div>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          { name: 'description', content: '@TODO' },
+          { name: 'keywords', content: '@TODO' },
+        ]}
+      >
+        {/* <link rel="preload" href={interUI} as="font" type="font/woff2" />
+        <link rel="preload" href={interUIBold} as="font" type="font/woff2" />
+        <link rel="preload" href={vollkorn} as="font" type="font/woff2" />
+        <link rel="preload" href={vollkornBold} as="font" type="font/woff2" /> */}
+      </Helmet>
+      {hasGrid && <BaselineGrid />}
+      <SiteHeader
+        titleHTML={data.site.siteMetadata.titleHTML}
+        navLinks={data.site.navLinks}
+      />
+      {children()}
+    </div>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.func,
