5a2c381cc9f5c65b1be3496afdbc7ff6b788f606
Jon Higgins <jon@jonhiggins.co.uk>
2018-06-02 16:56:37 +1000

    use page template for posts

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import PageWrapper from '~/src/components/pageWrapper'
 import Heading from '~/src/components/heading'
 
-const Page = ({ transition, heading }) => (
+const Page = ({ transition, heading, children }) => (
   <div style={transition && transition.style}>
     <PageWrapper>
       <Heading element={'h1'} marginTop={6} marginBottom={6} size={4}>
         {heading}
       </Heading>
+      {children}
     </PageWrapper>
   </div>
 )
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Page from '~/src/components/page'
 
-export default function Template({ data }) {
+export default function Template({ data, transition }) {
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <Page transition={transition} heading={frontmatter.title}>
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <h2>{frontmatter.date}</h2>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
+    </Page>
   )
+}
+
+Template.propTypes = {
+  transition: PropTypes.object,
+  data: PropTypes.object,
 }
 
 export const pageQuery = graphql`
